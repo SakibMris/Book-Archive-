a11d736b41142063fcfa121fa82e77e446a0041a
SakibMris <sakibmris@gmail.com>
2021-09-02 13:59:53 +0600

    error resolved

--- js/app.js
+++ js/app.js
@@ -1,22 +1,31 @@
 const getText = () => {
+    // getting user searching text 
     const input = document.getElementById('input-field');
     const inputValue = input.value;
+    // input box clear 
     input.value = "";
-    document.getElementById('search-result').innerHTML = "";
+    // serach result area clear 
+    document.getElementById('search-result').innerHTML = ""
+    // error area clear after search 
+    document.getElementById('error-handling').innerHTML = ""
+    // handling error when no input 
     if (inputValue === "") {
         document.getElementById('error-handling').innerHTML = `<h1 class="text-danger"> Please Input Book Name</h1>`
         return;
     }
     else {
+        // fetching data from server 
         const url = `https://openlibrary.org/search.json?q=${inputValue}`
         fetch(url)
             .then(res => res.json())
             .then(data => showDetails(data))
     }
 }
+// details showing function 
 const showDetails = data => {
     console.log(data.numFound);
     document.getElementById('error-handling').innerHTML = `<h1 class=" text-success p-3">Total Search Found:${data.numFound}</h1>`
+    // Error handling for wrong search input 
     if (data.numFound === 0) {
         document.getElementById('error-handling').innerHTML = `<h1 class="text-warning">No Result Found</h1>`
         return;
